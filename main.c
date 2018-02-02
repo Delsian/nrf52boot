c9c2ad9988c32181f47dbd00ac674c6f82c7772c
Eugene Krashtan <eugene.krashtan@anoto.com>
2018-02-02 18:41:56 +0200

    Initial bootloader variant

---
+++ main.c
@@ -0,0 +1,51 @@
+/**
+ * @defgroup bootloader_secure_ble main.c
+ */
+
+
+
+#include <stdint.h>
+#include "boards.h"
+#include "nrf_mbr.h"
+#include "nrf_bootloader.h"
+#include "nrf_bootloader_app_start.h"
+#include "nrf_dfu.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+#include "app_error.h"
+#include "app_error_weak.h"
+#include "nrf_bootloader_info.h"
+
+
+/**@brief Function for application main entry. */
+int main(void)
+{
+    uint32_t ret_val;
+
+    ret_val = nrf_bootloader_init();
+    APP_ERROR_CHECK(ret_val);
+
+    // Either there was no DFU functionality enabled in this project or the DFU module detected
+    // no ongoing DFU operation and found a valid main application.
+    // Boot the main application.
+    nrf_bootloader_app_start(MAIN_APPLICATION_START_ADDR);
+
+    // Should never be reached.
+    NRF_LOG_INFO("After main");
+}
+
+
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
+{
+    NRF_LOG_ERROR("Received a fault! id: 0x%08x, pc: 0x%08x, info: 0x%08x", id, pc, info);
+    NVIC_SystemReset();
+}
+
+
+void app_error_handler_bare(uint32_t error_code)
+{
+    (void)error_code;
+    NRF_LOG_ERROR("Received an error: 0x%08x!", error_code);
+    NVIC_SystemReset();
+}
