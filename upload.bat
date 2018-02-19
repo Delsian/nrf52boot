4bc5772d0e2d4a5b6495d8e03fc036755381c71c
Eugene Krashtan <eugene.krashtan@anoto.com>
2018-02-19 18:04:54 +0200

    Script fix

--- upload.bat
+++ upload.bat
@@ -3,7 +3,7 @@ setlocal
 
 set BOOT_HEX=Release/nrf52boot.hex
 set SD_HEX=../SDK/components/softdevice/s132/hex/s132_nrf52_5.0.0_softdevice.hex
-set ADV_NAME=../Config/dfuadvname.hex
+set ADV_NAME=../Config/settings.hex
 
 nrfjprog -f nrf52 --eraseall
 nrfjprog -f nrf52 --program %SD_HEX%
